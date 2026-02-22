f21e76cc95d3a8315f8b977c1365d0e225ab9188
Macys1704 <cherin1704@gmail.com>
2026-02-22 02:39:41 -0300

    Servidor na porta 8080 funcional

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('hello world');
+import express, { Request, Response } from "express" 
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get("/", (req: Request   , res: Response) => {
+     res.send("hellow world");
+});
+
+app.listen(port, () => console.log("Porta 8080 funcional"))
